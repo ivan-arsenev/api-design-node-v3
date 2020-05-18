ba36eb80a7a72c2df40db7e04665fad38efed28e
ivan-arsenev <ivan_arsenev@pm.me>
2020-05-18 22:11:38 +0300

    Added some changes

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -2,4 +2,21 @@ import { Router } from 'express'
 
 const router = Router()
 
+router
+  .get('/', (req, res) => {
+    res.send({ data: 'some item' })
+  })
+  .get('/:id', (req, res) => {
+    res.send({ data: 'by id' })
+  })
+  .post('/', (req, res) => {
+    res.send({ data: 'post' })
+  })
+  .delete('/:id', (req, res) => {
+    res.send({ data: 'delete' })
+  })
+  .put('/:id', (req, res) => {
+    res.send({ data: 'put' })
+  })
+
 export default router
